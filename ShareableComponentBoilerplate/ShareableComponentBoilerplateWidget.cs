a588407c6373f9b98bf23a63155bb3f5744b296f
tfayas@hbs.net <tfayas@hbs.net>
2019-07-28 23:57:20 -0500

    Initial Add

---
+++ ShareableComponentBoilerplate/ShareableComponentBoilerplateWidget.cs
@@ -0,0 +1,4 @@
+﻿using Kentico.PageBuilder.Web.Mvc;
+using ShareableComponentBoilerplate;
+
+[assembly: RegisterWidget("CompanyName.ShareableComponentBoilerplate", "Sample widget", typeof(ShareableComponentBoilerplateWidgetModel), customViewName: "Widgets/_ShareableComponentBoilerplate")]
---
+++ ShareableComponentBoilerplate/ShareableComponentBoilerplateWidgetModel.cs
@@ -0,0 +1,17 @@
+﻿using Kentico.Forms.Web.Mvc;
+using Kentico.PageBuilder.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShareableComponentBoilerplate
+{
+    public class ShareableComponentBoilerplateWidgetModel : IWidgetProperties
+    {
+        // Defines a property and sets its default value
+        [EditingComponent(TextInputComponent.IDENTIFIER, Order = 0, Label = "Text")]
+        public string TextValue { get; set; } = "Hello World";
+    }
+}
